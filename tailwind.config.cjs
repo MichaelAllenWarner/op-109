6af57091ce22b9997febf0e2537b9aafdda849dc
Michael Warner <michaelwarner@Michaels-MacBook-Pro-2.local>
2023-07-06 17:03:58 -0400

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+const typography = require("@tailwindcss/typography");
+
+/** @type {import('tailwindcss').Config}*/
+const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+
+  theme: {
+    extend: {}
+  },
+
+  plugins: [typography]
+};
+
+module.exports = config;
